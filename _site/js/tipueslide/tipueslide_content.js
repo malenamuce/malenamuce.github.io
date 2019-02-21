321ae56451a33ac09afaa4e0877658faf1f34a61
Zetinator <erick_zetina@hotmail.com>
2019-02-21 01:50:48 -0600

    taking away the trash

--- _site/js/tipueslide/tipueslide_content.js
+++
@@ -1,7 +0,0 @@
-
-var tipueslide = {"pages": [
-     {"title":"MARAZUL", "text": "Nosotros nosotros Ofertas ofertas Destacados destacados Venta venta Renta renta", "img": "https://scontent.fmex3-1.fna.fbcdn.net/v/t1.0-9/51604141_303903210482293_5286658855051198464_o.jpg?_nc_cat=100&_nc_ht=scontent.fmex3-1.fna&oh=f1bcb59b2969e1d6ca9af41ece4b1f88&oe=5CDD79E4", "url": "index.html"},
-     {"title":"Contacto", "text": "Contacto contacto. Suscríbete suscribete Ofertas ofertas especiales. Hilario Malpica No. 33, Fracc. Costa Azul, Acapulco Gro. Tel tel (744) 104 0619 Información información ? Correo correo male.muce@gmail.com", "note":"Ponte en contacto con nosotros", "img": "https://images.unsplash.com/uploads/1413222992504f1b734a6/1928e537?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80", "url": "index.html#contacto"},
-     {"title": "@marazul", "text": "facebook Facebook", "url": "https://www.facebook.com/casasmarazul/"},
-     {"title": "@marazul", "text": "twitter Twitter", "url": "https://www.facebook.com/casasmarazul/"}
-]};
